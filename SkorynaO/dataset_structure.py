54a2b84b987987a2c243ef5ee0e760aeacdd13c1
Oleksii-Skoryna <58769494+Oleksii-Skoryna@users.noreply.github.com>
2019-12-11 12:00:47 +0200

    Add files via upload

---
+++ SkorynaO/dataset_structure.py
@@ -0,0 +1,17 @@
+dataset = {
+    "337290" : {
+        "state":"NY",
+        "total_episodes_non_lupa":"3148",
+        "total_hha_charge_amount_non_lupa":"9482140"
+    },
+    "58217": {
+        "state": "CA",
+        "total_episodes_non_lupa": "620",
+        "total_hha_charge_amount_non_lupa": "1786721"
+    },
+    "58419": {
+        "state": "CA",
+        "total_episodes_non_lupa": "19",
+        "total_hha_charge_amount_non_lupa": "127300"
+    }
+}
